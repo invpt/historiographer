984a28d4848e1dcdcc05a4ea3da8e77357d03f40
invpt <57822954+invpt@users.noreply.github.com>
2024-06-22 00:38:44 -0400

    Nicer example program

--- main.go
+++ main.go
@@ -17,15 +17,16 @@ func main() {
 	//  \ /
 	//   z        (definition)
 	a := rt.Variable(0)
-	x := rt.Definition(func(dep higrt.Depend) any { return dep(a) })
-	y := rt.Definition(func(dep higrt.Depend) any { return dep(a) })
-	z := rt.Definition(func(dep higrt.Depend) any { return dep(x).(int) + dep(y).(int) })
+	x := rt.Definition(func(dep higrt.Depend) any { return dep(a).(int) * 2 })
+	y := rt.Definition(func(dep higrt.Depend) any { return dep(a).(int)*3 + 35 })
+	z := rt.Definition(func(dep higrt.Depend) any { return dep(x).(int) + dep(y).(int)*4 })
 
 	fmt.Println("Address of a:", a)
 	fmt.Println("Address of x:", x)
 	fmt.Println("Address of y:", y)
 	fmt.Println("Address of z:", z)
 
+	// Run some transactions
 	go (func() {
 		rt.Transact(
 			map[higact.Address]higrt.LockKind{a: higrt.LockKindWrite},
@@ -42,8 +43,29 @@ func main() {
 		)
 	})()
 
-	for zValue := range rt.Inspect(z) {
-		fmt.Println("New value of z:", zValue)
-	}
+	// Inspect some values
+	go (func() {
+		for value := range rt.Inspect(a) {
+			fmt.Println("New value of a:", value)
+		}
+	})()
+	go (func() {
+		for value := range rt.Inspect(x) {
+			fmt.Println("New value of x:", value)
+		}
+	})()
+	go (func() {
+		for value := range rt.Inspect(y) {
+			fmt.Println("New value of y:", value)
+		}
+	})()
+	go (func() {
+		for value := range rt.Inspect(z) {
+			fmt.Println("New value of z:", value)
+		}
+	})()
 
+	// Stop the program from stopping immediately
+	var unused string
+	fmt.Scanln(&unused)
 }
