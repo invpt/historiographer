bf30d2fcb28ee42a4677bd3c77c8f6f7a5e01699
invpt <57822954+invpt@users.noreply.github.com>
2024-06-22 00:28:24 -0400

    Organize example code for readability

--- main.go
+++ main.go
@@ -11,12 +11,13 @@ func main() {
 	rt := higrt.NewRuntime(higact.NewLocalRouter())
 
 	a := rt.Variable(0)
-	fmt.Println("Address of a:", a)
 	x := rt.Definition(func(dep higrt.Depend) any { return dep(a) })
-	fmt.Println("Address of x:", x)
 	y := rt.Definition(func(dep higrt.Depend) any { return dep(a) })
-	fmt.Println("Address of y:", y)
 	z := rt.Definition(func(dep higrt.Depend) any { return dep(x).(int) + dep(y).(int) })
+
+	fmt.Println("Address of a:", a)
+	fmt.Println("Address of x:", x)
+	fmt.Println("Address of y:", y)
 	fmt.Println("Address of z:", z)
 
 	go (func() {
